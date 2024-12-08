3df7628ee619f8d4f7a120a9c5f8aa85e6856ef5
vaijaaaaa <vaijuwalker111@gmail.com>
2024-12-08 23:17:07 +0530

    stuff

---
+++ src/models/name.ts
@@ -0,0 +1,7 @@
+export const db = "main-stackflow"
+export const questionCollection = "questions"
+export const answerCollection = "answers"
+export const CommentCollection = "comments"
+export const voteCollection = "votes"
+export const questionAttachmentBucket = "question-attachment"
+
